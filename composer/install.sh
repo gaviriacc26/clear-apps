1cc54f7f191fc8f6e4d3a2f0fde73bfdca62a6fc
Camilo Gaviria <camilogaviria2697@gmail.com>
2020-03-20 06:05:05 +0000

    Added missing steps to composer installer

--- composer/install.sh
+++ composer/install.sh
@@ -6,6 +6,12 @@ if [ "$EUID" -ne 0 ]
   exit
 fi
 
+# Install php
+swupd bundle-add php-basic
+
+# Create /usr/local/bin directory if it doesn't exist
+mkdir -p /usr/local/bin
+
 # Composer's installation instructions with some flags for ease of use in CL
 php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
 php -r "if (hash_file('sha384', 'composer-setup.php') === 'e0012edf3e80b6978849f5eff0d4b4e4c79ff1609dd1e613307e16318854d24ae64f26d17af3ef0bf7cfb710ca74755a') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
