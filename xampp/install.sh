9dbd9075b42f1fc8686406cb32e83252dc43c35c
Camilo Gaviria <camilogaviria2697@gmail.com>
2020-04-11 03:16:14 +0000

    xampp convinience

---
+++ xampp/install.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# Ensure script is run as root (sudo)
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
+# Create needed directories
+mkdir -p /usr/local/share/applications
+
+# Register policy to enable GUI after password prompt
+cp xampp.policy /usr/share/polkit-1/actions/
+
+# Create xampp gnome desktop app
+cp xampp.desktop /usr/local/share/applications
+
